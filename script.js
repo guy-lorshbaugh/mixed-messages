893519afb4a796bba82aa329f08539a8ebf899c9
Guy Lorshbaugh <67803858+guy-lorshbaugh@users.noreply.github.com>
2021-01-17 15:51:08 -0800

    made some formatting changes to output message

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 const characteristics = [
-    `is generally smaller than the African bush elephant and has the highest
-    body point on the head.`,
-    `male, when fully-grown, is about 2.75 m (9.0 ft) tall at the shoulder and
-    4 t (4.4 short tons) in weight, while females are smaller at about 2.40 m
-    (7.9 ft) at the shoulder and 2.7 t (3.0 short tons) in weight.`,
-    `trunk is an elongation of the nose and upper lip combined; the nostrils
-    are at its tip, which has a one finger-like process. The trunk contains
-    as many as 60,000 muscles, which consist of longitudinal and radiating sets.`,
-    `tusks serve to dig for water, salt, and rocks, to debark and uproot trees,
-    as levers for maneuvering fallen trees and branches, for work, for display,
-    for marking trees, as weapon for offence and defence, as trunk-rests, and
-    as protection for the trunk. Elephants are known to be right or left tusked.`,
-    `skin colour is usually grey, and may be masked by soil because of dusting
-    and wallowing. Their wrinkled skin is movable and contains many nerve centres.`,
-    `has a very large and highly developed neocortex, a trait also shared by
-    humans, apes and certain dolphin species. They have a greater volume of
-    cerebral cortex available for cognitive processing than all other existing
-    land animals.`
+    `is generally smaller than the African bush elephant 
+    and has the highest body point on the head.`,
+    `male, when fully-grown, is about 2.75 m (9.0 ft) tall
+    at the shoulder and 4 t (4.4 short tons) in weight, while females are
+    smaller at about 2.40 m (7.9 ft) at the shoulder and 2.7 t (3.0 short tons)
+    in weight.`,
+    `trunk is an elongation of the nose and upper lip 
+    combined; the nostrils are at its tip, which has a one finger-like process.
+    The trunk contains as many as 60,000 muscles, which consist of longitudinal
+    and radiating sets.`,
+    `tusks serve to dig for water, salt, and rocks, to debark
+    and uproot trees, as levers for maneuvering fallen trees and branches, for
+    work, for display, for marking trees, as weapon for offence and defence, as
+    trunk-rests, and as protection for the trunk. Elephants are known to be
+    right or left tusked.`,
+    `skin colour is usually grey, and may be masked by soil
+    because of dusting and wallowing. Their wrinkled skin is movable and
+    contains many nerve centres.`,
+    `The Asian elephant has a very large and highly developed neocortex, a trait
+    also shared by humans, apes and certain dolphin species. They have a greater
+    volume of cerebral cortex available for cognitive processing than all other
+    existing land animals.`
 ];
 const habitat = [
     `They inhabit grasslands, tropical evergreen forests, semi-evergreen forests,
@@ -53,32 +57,29 @@ const behavior = [
     their feet in conjunction with the trunk for manipulating objects.`
 ];
 
-const getIndex = array => {
+const getRandMsg = array => {
     index =  Math.floor(Math.random() * 6);
     return array[index];
 }
 
 const messageGenerator = () => {
-    const charMessage = getIndex(characteristics);
-    const habMessage = getIndex(habitat);
-    const behavMessage = getIndex(behavior);
+    const charMessage = getRandMsg(characteristics);
+    const habMessage = getRandMsg(habitat);
+    const behavMessage = getRandMsg(behavior);
     return `
     |-------------------------------------------|
     |  Here are your Elephant Facts for today!  |
     |-------------------------------------------|
 
-    CHARACTERISTICS:
-    The Asian elephant ${charMessage}
+    -- The Asian elephant ${charMessage}
 
-    HABITAT:
-    ${habMessage}
+    -- ${habMessage}
 
-    BEHAVIOR:
-    ${behavMessage}
-                                                                    
+    -- ${behavMessage}
+
     |---------------------------------------------------------------|
     |                Thank you for using Elephant Facts!            |
-    |  (Facts are compiled from the Asian Elephant Wikipedia page)  |
+    |  (Facts are compiled from the Asian Elephant Wikipedia page.) |
     |---------------------------------------------------------------|
                                 _    _
                                 /=\\""/=\\
